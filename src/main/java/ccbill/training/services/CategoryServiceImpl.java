574e343ebb7bd235314eaa2889e5940069a9b4db
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-05-05 11:02:50 +0200

    Added Category Controller and CategoryListDTO
    Lesson 381

--- src/main/java/ccbill/training/services/CategoryServiceImpl.java
+++ src/main/java/ccbill/training/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package ccbill.training.services;
 import ccbill.training.api.v1.mapper.CategoryMapper;
 import ccbill.training.api.v1.model.CategoryDTO;
 import ccbill.training.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
